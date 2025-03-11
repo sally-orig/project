5ce037113c8b31ec8d2d3713604bb1f97fdd5a94
sally-orig <sgano@77global.biz>
2025-03-11 16:03:36 +0800

    add model with sample data to db

--- VegiePriceMonitoring/PriceMonitor/models.py
+++ VegiePriceMonitoring/PriceMonitor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Vegetable(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_by = models.CharField(max_length=255)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    img = models.ImageField(upload_to='veg_images', blank=False, null=False)
+
+    def __str__(self):
+        return self.name
--- VegiePriceMonitoring/PriceMonitor/views.py
+++ VegiePriceMonitoring/PriceMonitor/views.py
@@ -1,13 +1,11 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import Vegetable
 
 def price_list(request):
+    vegetables = Vegetable.objects.all().values('name', 'price', 'updated_at', 'img')
     template = loader.get_template('pricelist.html')
     context = {
-        'vegetables': [
-            {'name': 'Tomato', 'price': 10.5, 'updated_at': '2021-10-01 12:00:00'},
-            {'name': 'Potato', 'price': 25, 'updated_at': '2021-10-01 12:00:00'},
-            {'name': 'Onion', 'price': 199, 'updated_at': '2021-10-01 12:00:00'}
-        ],
+        'vegetables': vegetables
     }
     return HttpResponse(template.render(context, request))
