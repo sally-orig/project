4a17862a4dbfb58f7ff249bd8def3a519d2b3652
sally-orig <sgano@77global.biz>
2025-03-11 14:40:25 +0800

    modify pricelist view

--- VegiePriceMonitoring/PriceMonitor/views.py
+++ VegiePriceMonitoring/PriceMonitor/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 def price_list(request):
     template = loader.get_template('pricelist.html')
     context = {
-        'data': 'Hello world!',
+        'vegetables': [
+            {'name': 'Tomato', 'price': 10.5, 'updated_at': '2021-10-01 12:00:00'},
+            {'name': 'Potato', 'price': 25, 'updated_at': '2021-10-01 12:00:00'},
+            {'name': 'Onion', 'price': 199, 'updated_at': '2021-10-01 12:00:00'}
+        ],
     }
     return HttpResponse(template.render(context, request))
